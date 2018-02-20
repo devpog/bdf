8405770256efc464c542a9f3caef36c66a321a47
devpog <devpog@gmail.com>
2018-02-20 00:32:29 -0500

    added gitignore

--- core/commodity.py
+++ core/commodity.py
@@ -4,5 +4,12 @@ import numpy as np
 
 class Commodity:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
